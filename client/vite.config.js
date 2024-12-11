fef3d3af7129ca0555343dab4c0a19c4629446fb
PRIYANKAS3110 <priyankasekarjaya@gmail.com>
2024-12-11 18:52:01 +0530

    nine

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,14 +6,10 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Ensure the output directory matches Vercel's requirements
     rollupOptions: {
-      onwarn(warning, handler) {
-        if (warning.code) {
-          console.warn(warning.message); // Optionally log the warning
-          return;
-        }
-        handler(warning);
-      },
-    },
+      external: [             
+        'node:*' 
+      ],
+  },
   },
   server: {
     port: 3000, // Optional: Set a default dev server port
